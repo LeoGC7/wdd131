095c04d3d694503ae694a12121dca822debc2c80
LeoGC7 <leonardo.gameiro.correa@gmail.com>
2024-10-31 10:40:52 -0300

    review added

--- scripts/form.js
+++ scripts/form.js
@@ -1,7 +1,6 @@
 const currentYear = new Date().getFullYear();
     document.getElementById("currentyear").textContent = currentYear;
 
-
 const lastModified = new Date(document.lastModified);
 
 const optionsDate = { 
@@ -21,10 +20,7 @@ const formattedDate = lastModified.toLocaleDateString('en-US', optionsDate);
 const formattedTime = lastModified.toLocaleTimeString('en-US', optionsTime);
 
 const dateContainer = document.getElementById("lastmodified");
-
 dateContainer.textContent = `Last modification: ${formattedDate} ${formattedTime}`;
-
-
 
 const products = [
     {
@@ -64,4 +60,40 @@ const products = [
         option.textContent = product.name;
         productSelect.appendChild(option);
     });
+});
+
+const stars = document.querySelectorAll('.form-radio');
+const starIcons = document.querySelectorAll('.star-icon');
+
+function updateStars(rating) {
+  starIcons.forEach((icon, index) => {
+      if (index < rating) {
+          icon.src = 'images/star-filled.png';
+      } else {
+          icon.src = 'images/star.png';
+      }
+  });
+}
+
+stars.forEach((star, index) => {
+  star.addEventListener('change', () => {
+      updateStars(index + 1);
+  });
+});
+
+// Review JS
+document.addEventListener('DOMContentLoaded', () => {
+  let submissionCount = localStorage.getItem('submissionCount');
+
+  if (!submissionCount) {
+      submissionCount = 0;
+  } else {
+      submissionCount = parseInt(submissionCount);
+  }
+
+  submissionCount += 1;
+
+  localStorage.setItem('submissionCount', submissionCount);
+
+  document.getElementById('sub-number').textContent = submissionCount;
 });
